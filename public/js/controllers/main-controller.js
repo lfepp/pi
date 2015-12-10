8d77e746324ba65e2cd3f41250bc260bcd9abafe
Luke Epp <lucasfepp@gmail.com>
2015-12-10 10:08:12 -0500

    Added ngChange for pi input

--- public/js/controllers/main-controller.js
+++ public/js/controllers/main-controller.js
@@ -1,8 +1,13 @@
 angular.module('MainCtrl', ['MainService']).controller('MainController', ['$scope', 'PiCheck', function($scope, PiCheck) {
 
   $scope.test = 'It works!';
-
+  $scope.digit = 0;
   $scope.piInput = '';
+
+  $scope.update = function() {
+    console.log(PiCheck.checkDigit($scope.digit));
+    $scope.digit++;
+  };
 
   console.log(PiCheck.test());
 
