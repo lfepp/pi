bcfb06390fd52415c0c8d90bd27e118f085b29d4
Luke Epp <lucasfepp@gmail.com>
2015-12-14 20:41:21 -0500

    Added connection to MongoDB for scoreboard

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var secrets = require('./app/config/secrets.js');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-// require(__dirname + '/public/js/calculate-pi.js');
+mongoose.connect(secrets.db.url);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/views/index.html');
